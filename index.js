c629b485d15609309d1adf2ee6965a5471f000fd
LProgead <lprogead@mailo.com>
2020-07-30 20:34:38 +0200

    Modification de l'index.js
    Modification de l'index.js dans un but d'optimisation du temps de réponse.

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 const a = dayjs();
 
-
-
 client.login(process.env.token);
 
 client.on('ready', () => {
@@ -36,139 +34,9 @@ client.on('message', message => {
                 if (!element[category]) return;
                 element[category].forEach(keyword => {
                     if (keyword === word) {
-                        switch (category) {
-                            case "salutation":
-                                message.channel.send("Bien le bonjour très cher compatriote.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "costard":
-                                message.channel.send("La meilleure façon de se payer un costard, c'est de travailler.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "milliardaire":
-                                message.channel.send("Il faut des jeunes Français qui aient envie de devenir milliardaires.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "covid":
-                                message.channel.send("La France unie, c'est notre meilleur atout dans la période troublée par la COVID-19 que nous traversons. Nous tiendrons. Tous ensemble.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "kwassa-kwassa":
-                                message.channel.send("Le kwassa-kwassa pêche peu, il amène du Comorien.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "gaulois":
-                                message.channel.send("Vous êtes des Gaulois réfractaires au changement.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "manif":
-                                message.channel.send("Je ne céderai rien ni aux fainéants, ni aux cyniques, ni aux extrêmes.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "chômeur":
-                                message.channel.send("Je traverse la rue, je vous trouve du travail.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "sextape":
-                                message.channel.send("Tu fais ce que tu veux. C’est ta vie, ta famille qui est en jeu.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "retraite":
-                                message.channel.send("On pouvait parler très librement, la chose que l’on n’avait pas le droit de faire c’est de se plaindre. Je trouve que c’est une bonne pratique qu’avait le Général, le pays se tiendrait autrement si c’était comme cela. On ne se rend pas compte de la chance que l’on a. On vit de plus en plus vieux et en bonne santé.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "syndicat":
-                                message.channel.send("Je mesure les craintes et l'opposition mais il y a aussi beaucoup de mensonges et de manipulation.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "reelu":
-                                message.channel.send("Il ne reste que " + a.to('2022-04-23', true).replace('years', 'années').replace('year', 'année').replace('months', 'mois').replace('month', 'mois').replace('days', 'jours').replace('day', 'jour').replace('hours', 'heures').replace('hour', 'heure').replace(' a ', ' 1 ').replace('in', 'dans') + " avant ma victoire aux prochaines présidentielles.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "perlimpimpin":
-                                message.channel.send("Ce que vous proposez, comme d'habitude, c'est de la poudre de Perlimpinpin.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "bigard":
-                                message.channel.send("Je suis fâché.");
-                                answered = 1;
-                                return;           
-                            break;
-
-                            case "t+":
-                                message.channel.send("Les salariés français sont trop payés... Les salariés doivent pouvoir travailler plus, sans être payés plus si les syndicats majoritaires sont d’accord.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "entrepreneur":
-                                message.channel.send("Je dis aux jeunes : “ne cherchez plus un patron cherchez des clients”.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "roi":
-                                message.channel.send("La France est en deuil d'un roi.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "apprenti":
-                                message.channel.send("Je compte sur vous pour engager plus d’apprentis. C’est désormais gratuit quand ils sont mineurs.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "jeune":
-                                message.channel.send("Quand on est jeune, 35 heures ce n'est pas assez.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "car":
-                                message.channel.send("Les pauvres pourront voyager plus facilement.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "gare":
-                                message.channel.send("Une gare, c’est un lieu où l’on croise les gens qui réussissent et les gens qui ne sont rien.");
-                                answered = 1;
-                                return;
-                            break;
-
-                            case "pauvre":
-                                message.channel.send("Les gens naissent pauvres et restent pauvres. Ceux qui tombent pauvres restent pauvres... Il faut qu'ils puissent s'en sortir.");
-                                answered = 1;
-                                return;
-                            break;
-                        }
+                        message.channel.send(`${element[category + `_answer`]}`);
+                        answered = 1;
+                        return;
                     }
                 });
             });
