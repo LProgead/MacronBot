9432c7f7644a1685b1d88d805f39bf082219b66e
LProgead <lprogead@mailo.com>
2020-08-16 21:50:58 +0200

    Correction d'un bug + ajouts des stats
    Correction du bug de la détection de "manu" et ajouts de statistiques de bases.

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-require('dotenv').config()
-const keywords = require('./keywords.json');
-const categories = ["salutation", "costard", "milliardaire", "covid", "kwassa-kwassa", "gaulois", "manif", "chômeur", "sextape", "retraite", "syndicat", "reelu", "perlimpimpin", "bigard", "t+", "entrepreneur", "roi", "apprenti", "jeune", "car", "gare", "pauvre", "réélu", "vrvf", "démission"];
-const dayjs = require ('dayjs');
+require('dotenv').config();
+const fs = require('fs');
+const stats = require('./stats.json');
+const keywords = require( './keywords.json');
+const categories = ["manu", "ping", "salutation", "costard", "milliardaire", "covid", "kwassa-kwassa", "gaulois", "manif", "chômeur", "sextape", "retraite", "syndicat", "reelu", "perlimpimpin", "bigard", "t+", "entrepreneur", "roi", "apprenti", "jeune", "car", "gare", "pauvre", "réélu", "vrvf", "démission", "amoureux"];
+const dayjs = require('dayjs');
 const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 const a = dayjs();
 
 client.login(process.env.token);
 
-client.on('ready', () => {
+client.on('ready', () => {   
     client.user.setActivity(a.to('2022-04-23', true).replace('years', 'années').replace('year', 'année').replace('months', 'mois').replace('month', 'mois').replace('days', 'jours').replace('day', 'jour').replace('hours', 'heures').replace('hour', 'heure').replace(' a ', ' 1 ').replace('in', 'dans') + " avant ma victoire aux prochaines présidentielles", { type : 'WATCHING' });
     setInterval(function() { client.user.setActivity(a.to('2022-04-23', true).replace('years', 'années').replace('year', 'année').replace('months', 'mois').replace('month', 'mois').replace('days', 'jours').replace('day', 'jour').replace('hours', 'heures').replace('hour', 'heure').replace(' a ', ' 1 ').replace('in', 'dans') + " avant ma victoire aux prochaines présidentielles", { type : 'WATCHING' }); }, 60000);
-    
+
+    let keywords_stats = 0;
+    categories.forEach(category => {
+        keywords.keywords.forEach(element => {
+            if (!element[category]) return;
+            keywords_stats += element[category].length;
+        });
+    });
+
+    let sentences_stats = 0;
+    categories.forEach(category => {
+        keywords.keywords.forEach(element => {
+            if (!element[category]) return;
+            sentences_stats += element[category + `_answer`].length;
+        });
+    });
+
+    stats.stats[0].keywords = keywords_stats;
+    stats.stats[0].sentences = sentences_stats;
+    stats.stats[0].categories = categories.length;
+
+    fs.writeFileSync('stats.json', JSON.stringify(stats));
+
     console.log(`[DÉMARRAGE] Bot lancé. Disponible sur ${client.guilds.cache.size} serveurs avec ${client.users.cache.size} utilisateurs.`);
     client.guilds.cache.forEach(guild => {
-	console.log(`${guild.name} dirigé par ${guild.owner.user.tag}.`);
+	    console.log(`${guild.name} dirigé par ${guild.owner.user.tag}.`);
     });
 });
 
 client.on('message', message => {
     if (message.author.bot) return;
-
-    if (message.content.includes('737659120219586652')) return message.channel.send("Mon cher compatriote, bonjour ! \nJe suis très heureux que tu aies remarqué ma présence sur ton serveur. \nPour te remercier de la considération que tu portes à ton Président de la République, je vais te confier un immense secret : mon fonctionnement. Tu peux tout retrouver sur un document Evernote rédigé par Zibeth-ma-copine avant que Castex ne lui fasse prendre la porte : https://www.evernote.com/shard/s747/sh/84a0652a-8bae-4aad-9966-e43bce06651d/30e65a53aaf847f95d387cab8685def6")
     
-    if (message.content.toLocaleLowerCase().includes(' manu ')) return message.channel.send("Tu m'appelles Monsieur le président de la République, ou Monsieur. D'accord ?");
-
     let answered = 0;
+
+    if (message.content.toLocaleLowerCase().replace('.', '').replace(',', '').split(' ').includes('manu')) {
+        message.channel.send("Tu m'appelles Monsieur le président de la République, ou Monsieur. D'accord ?");
+
+        answered = 1;
+    }
 
     message.content.toLocaleLowerCase().replace('.', '').replace(',', '').split(' ').forEach(word => {
         if (answered === 1) return;
@@ -39,6 +65,9 @@ client.on('message', message => {
                     if (keyword === word) {
                         message.channel.send(`${element[category + `_answer`][Math.floor(Math.random() * Math.floor(element[category + `_answer`].length))]}`);
                         answered = 1;
+                        stats.stats[0].answered++;
+
+                        fs.writeFileSync('stats.json', JSON.stringify(stats));
                         return;
                     }
                 });
